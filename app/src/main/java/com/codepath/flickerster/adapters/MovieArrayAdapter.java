858c3f685c56e46f7a584c758dde6b0059d6ea7e
Clara Liu <lsyang@pinterest.com>
2017-03-09 21:11:03 -0800

    added title subtitle and picture on item layout

--- app/src/main/java/com/codepath/flickerster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/flickerster/adapters/MovieArrayAdapter.java
@@ -1,9 +1,17 @@
 package com.codepath.flickerster.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.codepath.flickerster.R;
 import com.codepath.flickerster.models.Movie;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -17,4 +25,33 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         super(context, android.R.layout.simple_list_item_1, movies);
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // get the data item for position
+        Movie movie = getItem(position);
+
+        // check the existing view being used
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView =  inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // find the image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // clear out image from convertView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+
+        // return to view
+        return convertView;
+    }
 }
